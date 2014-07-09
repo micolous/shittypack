d4c92e2c8fa2f0d52205689ebc41e968e2f34632
Michael Farrell <micolous+git@gmail.com>
2014-07-09 23:48:33 +1000

    Initial commit of actual code

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
-shittypack
-==========
+# shittypack #
 
-Repacks verbose GTFS feeds (like Transport NSW's) so that they consume less disk space.
+Repacks verbose GTFS feeds so that they consume less disk space.
+
+Created for [Transport NSW's GTFS feeds](https://tdx.transportnsw.info/) which are lazily converted from TransXchange format, and needlessly verbose.
+
+## What it does ##
+
+- Removes Unicode byte order markers.
+- Removes unneeded quotation characters on CSV fields.
+- Rounds shape and stop latitude and longitude to 6 decimal places.
+- Rounds trip distances to 1 decimal place.
+- Rewrites the `shape_id`, `route_id`, `trip_id` and `service_id` fields so that they use incremental numeric values.
+
+## Usage ##
+
+This requires that you pass the files in this order:
+
+1. `shapes.txt`
+2. `routes.txt`
+3. `calendar.txt`
+4. `calendar_dates.txt` (if present)
+5. `trips.txt`
+6. `stop_times.txt`
+7. `stops.txt`
+7. All other files.
+
+They need to be done all in one shot, as existing IDs need to be remapped on the fly.  For example:
+
+```console
+$ python shittypack.py shapes.txt routes.txt calendar.txt calendar_dates.txt trips.txt stop_times.txt agency.txt stops.txt -o output/
+```
+
+The output directory (`-o`) will be created, and this tool will not run if the output directory already exists.  This is a safety mechanism so it doesn't overwrite files.
+
+This tool is pretty unforgiving about problems (and shit).  It will probably give you bad output if it fails, and not complain about it.
+
+If you don't specify the files in the correct order, you're going to have a bad time.
+
+## Results -- Transport NSW "full Greater Sydney data", 2014-07-07. ##
+
+### Source data ###
+
+- Uncompressed: 938 MiB
+- Compressed ZIP (as supplied): 206 MiB
+
+```
+-rw-rw-rw-@ 1 michael  staff       4320  7 Jul 11:51 agency.txt
+-rw-rw-rw-@ 1 michael  staff      46436  7 Jul 11:51 calendar.txt
+-rw-rw-rw-@ 1 michael  staff     102093  7 Jul 11:51 calendar_dates.txt
+-rw-rw-rw-@ 1 michael  staff     153752  7 Jul 11:51 routes.txt
+-rw-rw-rw-@ 1 michael  staff  289544136  7 Jul 11:51 shapes.txt
+-rw-rw-rw-@ 1 michael  staff  671119136  7 Jul 11:51 stop_times.txt
+-rw-rw-rw-@ 1 michael  staff    3475362  7 Jul 11:51 stops.txt
+-rw-rw-rw-@ 1 michael  staff   18790239  7 Jul 11:51 trips.txt
+```
+
+### Shitty-packed data ###
+
+- Uncompressed: 458 MiB (49% of original)
+- Compressed ZIP (zip -9): 111 MiB (54% of original)
+
+```
+-rw-r--r--+ 1 michael  staff       3779  9 Jul 23:37 agency.txt
+-rw-r--r--+ 1 michael  staff      26804  9 Jul 23:36 calendar.txt
+-rw-r--r--+ 1 michael  staff      56381  9 Jul 23:36 calendar_dates.txt
+-rw-r--r--+ 1 michael  staff     121597  9 Jul 23:36 routes.txt
+-rw-r--r--+ 1 michael  staff  137854807  9 Jul 23:36 shapes.txt
+-rw-r--r--+ 1 michael  staff  332237581  9 Jul 23:37 stop_times.txt
+-rw-r--r--+ 1 michael  staff    2460802  9 Jul 23:37 stops.txt
+-rw-r--r--+ 1 michael  staff    7211783  9 Jul 23:36 trips.txt
+```
---
+++ shittypack.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+"""
+shittypack.py - Repacks GTFS data so that it is more compact.
+
+Transport NSW's dataset is so verbose because it is converted from TransExchange.
+
+Copyright 2014 Michael Farrell <http://micolous.id.au/>; BSD 3-clause
+
+File order (as files must be in the correct order else this will break!)
+
+shapes.txt
+routes.txt
+calendar.txt
+calendar_dates.txt
+trips.txt
+stop_times.txt
+other files
+
+
+eg:
+python repack.py shapes.txt routes.txt calendar.txt calendar_dates.txt trips.txt stop_times.txt agency.txt stops.txt -o output/
+
+"""
+
+import argparse, csv, os, io
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('csv', nargs='+')
+parser.add_argument('-o', '--output-dir', required=True)
+
+options = parser.parse_args()
+
+os.mkdir(options.output_dir)
+
+# Remap the shape, route, service and trip IDs to simple numbers.
+# This is a very simple algo, it could do Hoffman encoding to make it better, but it's
+# probably not worth it.
+shape_map = {}
+last_shape_id = 0
+
+route_map = {}
+last_route_id = 0
+
+service_map = {}
+last_service_id = 0
+
+trip_map = {}
+last_trip_id = 0
+
+for fn in options.csv:
+	# Windows puts in Unicode BOM.  This will swallow it if it is present.
+	input_f = io.open(fn, 'r', encoding='utf-8-sig')
+	output_f = open(os.path.join(options.output_dir, fn), 'wb')
+
+	c = csv.reader(input_f)
+	oc = csv.writer(output_f)
+
+	header = c.next()
+	oc.writerow(header)
+
+	if fn.lower().endswith('shapes.txt'):
+		# shapes.txt
+		# Clamp lat/long to 6 decimal places
+		# Clamp trip distance to 1 decimal place
+		id = header.index('shape_id')
+		lat = header.index('shape_pt_lat')
+		lng = header.index('shape_pt_lon')
+		dst = header.index('shape_dist_traveled')
+
+		for row in c:
+			# clamp decimal places
+			if '.' in row[lat]:
+				row[lat] = row[lat][:row[lat].index('.')+7]
+			if '.' in row[lng]:
+				row[lng] = row[lng][:row[lng].index('.')+7]
+			if '.' in row[dst]:
+				row[dst] = row[dst][:row[dst].index('.')+2]
+
+			# remap shapes to numbers
+			if row[id] not in shape_map:
+				shape_map[row[id]] = str(last_shape_id)
+				last_shape_id += 1
+
+			row[id] = shape_map[row[id]]
+
+			oc.writerow(row)
+	elif fn.lower().endswith('routes.txt'):
+		id = header.index('route_id')
+		for row in c:
+			# remap routes to numbers
+
+			if row[id] not in route_map:
+				route_map[row[id]] = str(last_route_id)
+				last_route_id += 1
+
+			row[id] = route_map[row[id]]
+
+			oc.writerow(row)
+			
+	elif fn.lower().endswith('calendar.txt'):
+		id = header.index('service_id')
+		for row in c:
+			# remap services to numbers
+
+			if row[id] not in service_map:
+				service_map[row[id]] = str(last_service_id)
+				last_service_id += 1
+
+			row[id] = service_map[row[id]]
+
+			oc.writerow(row)
+
+	elif fn.lower().endswith('calendar_dates.txt'):
+		id = header.index('service_id')
+		for row in c:
+			# remap services to numbers
+			row[id] = service_map[row[id]]
+
+			oc.writerow(row)
+
+	elif fn.lower().endswith('trips.txt'):
+		rid = header.index('route_id')
+		eid = header.index('service_id')
+		tid = header.index('trip_id')
+		hid = header.index('shape_id')
+		for row in c:
+			# remap services to numbers
+
+			if row[tid] not in trip_map:
+				trip_map[row[tid]] = str(last_trip_id)
+				last_trip_id += 1
+
+			row[tid] = trip_map[row[tid]]
+			row[rid] = route_map[row[rid]]
+			row[eid] = service_map[row[eid]]
+			row[hid] = shape_map[row[hid]]
+
+			oc.writerow(row)
+
+
+	elif fn.lower().endswith('stop_times.txt'):
+		# stop_times.txt
+		# Clamp trip distance to 1 decimal place
+		dst = header.index('shape_dist_traveled')
+		id = header.index('trip_id')
+		for row in c:
+			if '.' in row[dst]:
+				row[dst] = row[dst][:row[dst].index('.')+2]
+
+			row[id] = trip_map[row[id]]
+
+			oc.writerow(row)
+
+	elif fn.lower().endswith('stops.txt'):
+		# stops.txt
+		# Clamp stop lat/long to 6 decimal places
+		lat = header.index('stop_lat')
+		lng = header.index('stop_lon')
+
+		for row in c:
+			if '.' in row[lat]:
+				row[lat] = row[lat][:row[lat].index('.')+7]
+			if '.' in row[lng]:
+				row[lng] = row[lng][:row[lng].index('.')+7]
+
+			oc.writerow(row)
+
+
+	else:
+		# Other files -- this normally just removes un-needed quotation characters from the
+		# CSV file
+		for row in c:
+			oc.writerow(row)
+
+	output_f.close()
+	input_f.close()
