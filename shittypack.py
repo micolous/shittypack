df1600b79fdb90c48734c0ce417ae6deb139965e
Michael Farrell <micolous+git@gmail.com>
2015-02-01 23:27:38 +1100

    Major refactoring for working with ZIP files insetad (closes #1)

--- shittypack.py
+++ shittypack.py
@@ -22,33 +22,17 @@ python repack.py shapes.txt routes.txt calendar.txt calendar_dates.txt trips.txt
 
 """
 
-import argparse, csv, os
+import argparse, csv, os, zipfile
 
-parser = argparse.ArgumentParser()
+try:
+	import cStringIO as StringIO
+except ImportError:
+	import StringIO
 
-parser.add_argument('csv', nargs='+')
-parser.add_argument('-o', '--output-dir', required=True)
+# Special files that need to be processed first.
 
-options = parser.parse_args()
 
-os.mkdir(options.output_dir)
-
-# Remap the shape, route, service and trip IDs to simple numbers.
-# This is a very simple algo, it could do Hoffman encoding to make it better, but it's
-# probably not worth it.
-shape_map = {}
-last_shape_id = 0
-
-route_map = {}
-last_route_id = 0
-
-service_map = {}
-last_service_id = 0
-
-trip_map = {}
-last_trip_id = 0
-
-def swallow_windows_unicode(fileobj):
+def swallow_windows_unicode(fileobj, rewind=True):
 	"""
 	Windows programs (specifically, Notepad) puts '\xef\xbb\xbf' at the start of
 	a Unicode text file.  This is used to handle "utf-8-sig" files.
@@ -58,7 +42,9 @@ def swallow_windows_unicode(fileobj):
 
 	Returns True if the characters were present.
 	"""
-	pos = fileobj.tell()
+	if rewind:
+		pos = fileobj.tell()
+
 	try:
 		bom = fileobj.read(3)
 	except:
@@ -68,25 +54,69 @@ def swallow_windows_unicode(fileobj):
 		return True
 
 	# Bytes not present, rewind the stream
-	fileobj.seek(pos)
+	if rewind:
+		fileobj.seek(pos)
 	return False
 
 
-for fn in options.csv:
-	# Windows puts in Unicode BOM.  This will swallow it if it is present.
-	#input_f = io.open(fn, 'r', encoding='utf-8-sig')
-	input_f = open(fn, 'rb')
-	swallow_windows_unicode(input_f)
-	output_f = open(os.path.join(options.output_dir, fn), 'wb')
+class ShittyPacker(object):
 
-	c = csv.reader(input_f)
-	oc = csv.writer(output_f)
+	def __init__(self, input_zip, output_zip):
+		SPECIAL_NAMES = [
+			('shapes.txt', self._f_shapes),
+			('routes.txt', self._f_routes),
+			('calendar.txt', self._f_calendar),
+			('calendar_dates.txt', self._f_calendar_dates),
+			('trips.txt', self._f_trips),
+			('stop_times.txt', self._f_stop_times),
+			('stops.txt', self._f_stops),
+		]
 
-	header = c.next()
-	oc.writerow(header)
+		# Remap the shape, route, service and trip IDs to simple numbers.
+		# This is a very simple algo, it could do Hoffman encoding to make it
+		# better, but it's probably not worth it.
+		self.shape_map = {}
+		self.last_shape_id = 0
 
-	if fn.lower().endswith('shapes.txt'):
-		# shapes.txt
+		self.route_map = {}
+		self.last_route_id = 0
+
+		self.service_map = {}
+		self.last_service_id = 0
+
+		self.trip_map = {}
+		self.last_trip_id = 0
+
+		self.zip = zipfile.ZipFile(input_zip, 'r')
+		self.out = zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED)
+		names = set(self.zip.namelist())
+
+		for fn, processor in SPECIAL_NAMES:
+			if fn not in names:
+				print "WARNING: Could not find %r" % fn
+				continue
+
+			outf = StringIO.StringIO()
+			oc = csv.writer(outf)
+			c, header = self._open_csv(fn)
+			oc.writerow(header)
+			processor(header, c, oc)
+			self.out.writestr(fn, outf.getvalue())
+
+		# Now work out what's left
+		for fn in names - set((x[0] for x in SPECIAL_NAMES)):
+			outf = StringIO.StringIO()
+			oc = csv.writer(outf)
+			c, header = self._open_csv(fn)
+			oc.writerow(header)
+			for r in c:
+				oc.writerow(r)
+			self.out.writestr(fn, outf.getvalue())
+
+	def _f_shapes(self, header, c, oc):
+		"""
+		Rewrite shapes.txt.
+		"""
 		# Clamp lat/long to 6 decimal places
 		# Clamp trip distance to 1 decimal place
 		id = header.index('shape_id')
@@ -104,48 +134,62 @@ for fn in options.csv:
 				row[dst] = row[dst][:row[dst].index('.')+2]
 
 			# remap shapes to numbers
-			if row[id] not in shape_map:
-				shape_map[row[id]] = str(last_shape_id)
-				last_shape_id += 1
+			if row[id] not in self.shape_map:
+				self.shape_map[row[id]] = str(self.last_shape_id)
+				self.last_shape_id += 1
 
-			row[id] = shape_map[row[id]]
+			row[id] = self.shape_map[row[id]]
 
 			oc.writerow(row)
-	elif fn.lower().endswith('routes.txt'):
+
+
+	def _f_routes(self, header, c, oc):
+		"""
+		Rewrite routes.txt
+		"""
 		id = header.index('route_id')
 		for row in c:
 			# remap routes to numbers
 
-			if row[id] not in route_map:
-				route_map[row[id]] = str(last_route_id)
-				last_route_id += 1
+			if row[id] not in self.route_map:
+				self.route_map[row[id]] = str(self.last_route_id)
+				self.last_route_id += 1
 
-			row[id] = route_map[row[id]]
+			row[id] = self.route_map[row[id]]
 
 			oc.writerow(row)
-			
-	elif fn.lower().endswith('calendar.txt'):
+
+	def _f_calendar(self, header, c, oc):
+		"""
+		Rewrite calendar.txt.
+		"""
 		id = header.index('service_id')
 		for row in c:
 			# remap services to numbers
 
-			if row[id] not in service_map:
-				service_map[row[id]] = str(last_service_id)
-				last_service_id += 1
+			if row[id] not in self.service_map:
+				self.service_map[row[id]] = str(self.last_service_id)
+				self.last_service_id += 1
 
-			row[id] = service_map[row[id]]
+			row[id] = self.service_map[row[id]]
 
 			oc.writerow(row)
 
-	elif fn.lower().endswith('calendar_dates.txt'):
+	def _f_calendar_dates(self, header, c, oc):
+		"""
+		Rewrite calendar_dates.txt.
+		"""
 		id = header.index('service_id')
 		for row in c:
 			# remap services to numbers
-			row[id] = service_map[row[id]]
-
+			row[id] = self.service_map[row[id]]
 			oc.writerow(row)
 
-	elif fn.lower().endswith('trips.txt'):
+
+	def _f_trips(self, header, c, oc):
+		"""
+		Rewrite trips.txt.
+		"""
 		rid = header.index('route_id')
 		eid = header.index('service_id')
 		tid = header.index('trip_id')
@@ -153,20 +197,21 @@ for fn in options.csv:
 		for row in c:
 			# remap services to numbers
 
-			if row[tid] not in trip_map:
-				trip_map[row[tid]] = str(last_trip_id)
-				last_trip_id += 1
+			if row[tid] not in self.trip_map:
+				self.trip_map[row[tid]] = str(self.last_trip_id)
+				self.last_trip_id += 1
 
-			row[tid] = trip_map[row[tid]]
-			row[rid] = route_map[row[rid]]
-			row[eid] = service_map[row[eid]]
-			row[hid] = shape_map[row[hid]]
+			row[tid] = self.trip_map[row[tid]]
+			row[rid] = self.route_map[row[rid]]
+			row[eid] = self.service_map[row[eid]]
+			row[hid] = self.shape_map[row[hid]]
 
 			oc.writerow(row)
 
-
-	elif fn.lower().endswith('stop_times.txt'):
-		# stop_times.txt
+	def _f_stop_times(self, header, c, oc):
+		"""
+		Rewrite stop_times.txt.
+		"""
 		# Clamp trip distance to 1 decimal place
 		dst = header.index('shape_dist_traveled')
 		id = header.index('trip_id')
@@ -174,12 +219,15 @@ for fn in options.csv:
 			if '.' in row[dst]:
 				row[dst] = row[dst][:row[dst].index('.')+2]
 
-			row[id] = trip_map[row[id]]
+			row[id] = self.trip_map[row[id]]
 
 			oc.writerow(row)
 
-	elif fn.lower().endswith('stops.txt'):
-		# stops.txt
+
+	def _f_stops(self, header, c, oc):
+		"""
+		Rewrite stops.txt.
+		"""
 		# Clamp stop lat/long to 6 decimal places
 		lat = header.index('stop_lat')
 		lng = header.index('stop_lon')
@@ -193,11 +241,31 @@ for fn in options.csv:
 			oc.writerow(row)
 
 
-	else:
-		# Other files -- this normally just removes un-needed quotation characters from the
-		# CSV file
-		for row in c:
-			oc.writerow(row)
+	def _open_csv(self, filename):
+		"""
+		Opens a given CSV file in the archive.
+		Returns tuple of (csvobj, header)
+		"""
+		input_f = self.zip.open(filename, 'r')
+		swallowed = swallow_windows_unicode(input_f, False)
+		if not swallowed:
+			# We need to manually reset the file.
+			input_f = self.zip.open(filename, 'r')
+		c = csv.reader(input_f)
+		header = c.next()
+		return c, header
 
-	output_f.close()
-	input_f.close()
+def main():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument('zip', nargs=1)
+	parser.add_argument('-o', '--output-zip', required=True)
+
+	options = parser.parse_args()
+	packer = ShittyPacker(options.zip[0], options.output_zip)
+	
+
+
+
+if __name__ == '__main__':
+	main()
